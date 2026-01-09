99b6b7494d8af31d2fb5f1fbdaca62129ce1eea4
cam williams <cam.minimal@gmail.com>
2026-01-09 10:35:45 -0500

    minor update

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const mainRouter = require("./routes/index");
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const mainRouter = require("./routes/index");
 
 const { PORT = 3001 } = process.env;
 
